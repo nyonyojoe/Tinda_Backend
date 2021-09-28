62b378ec91666836b75e4a67d5c16d9dc7a6c824
nyonyojoe <nyonyojoe@gmail.com>
2021-09-28 13:41:24 -0400

    tinda backend completed

--- server.js
+++ server.js
@@ -22,8 +22,19 @@ app.use(Cors());
 // Api Endpoints
 
 app.get('/',(req, res) => {
-    res.send('I will make 10000 a month by the end of the year')
+   res.send("Do it mother Fucker")
+})      
+
+app.get('/tinda/card',(req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else{
+            res.status(201).send(data)
+        }
+    })
 })
+
 
 app.post('/tinda/cards', (req, res) => {
     const dbCard = req.body;
@@ -36,14 +47,5 @@ app.post('/tinda/cards', (req, res) => {
     })
 });
 
-app.get("tinda/cards", (req, res) => {
-    Cards.find((err, data) => {
-        if(err) {
-            res.status(500).send(err)
-        } else {
-            res.status(200).send(data)
-        }
-    })
-})
 // Listener
 app.listen(port, () => console.log(`Server started on port: ${port}`))
